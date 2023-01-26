5ae73d4b6cc97831dd38600673b6016b27f73f05
Kennedy-karuri <kennkaruri99@gmail.com>
2023-01-26 12:20:48 +0300

    modified index.html

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple-calculator</title>
+</head>
+<body>
+    <h1>SIMPLE CALCULATOR</h1>
+    <form action="post">
+        <label for="Num1">
+      first number  
+<input type="number" name="Num1"  placeholder="Input your integer here">
+</label>
+
+<label for="num2">
+Second-number
+
+<input type="number" name="Num2"  placeholder="Input your integer here">
+
+</label>
+
+<button action ="post"> + </button>
+<button action ="post"> - </button>
+<button action ="post"> * </button>
+<button action ="post"> / </button>
+
+<label for="Result">
+ Result<input type="number" name="Result" placeholder="Result to be displayed here" >
+</label>
+
+
+
+    </form>
+
+</body>
+</html>
