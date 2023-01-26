7fbb93ddd400b700e6a750148c504e5b2c0f23f2
Christine Bai <bai20726@gmail.com>
2023-01-26 12:16:19 +0300

    created the class functions

---
+++ assets/classes/classes.php
@@ -0,0 +1,19 @@
+<?php
+
+class MyCalculator
+{
+    private $first_num;
+    private $second_num;
+
+    public function __construct($first_num, $second_num)
+    {
+        $this->first_num = $first_num;
+        $this->second_num = $second_num;
+    }
+}
+?>
+
+
+   
+
+  
