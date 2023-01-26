9c6345be6f1bb95dfc9d183254b2a5133c1bc900
Christine Bai <bai20726@gmail.com>
2023-01-26 14:12:49 +0300

    commit

--- assets/classes/classes.php
+++ assets/classes/classes.php
@@ -1,31 +1,4 @@
-<?php
 
-<<<<<<< HEAD
-class MyCalculator
-{
-    private $first_num;
-    private $second_num;
-
-    public function __construct($first_num, $second_num)
-    {
-        $this->first_num = $first_num;
-        $this->second_num = $second_num;
-    }
-            public function add() {
-                return $this->first_num + $this->second_num;
-                }
-            public function subtract() {
-                return $this->first_num - $this->second_num;
-                }
-
-            public function multiply() {
-                return $this->first_num * $this->second_num;
-                }
-            public function divide() {
-                return $this->first_num / $this->second_num;
-                    }
-=======
-?>
 
 
 
