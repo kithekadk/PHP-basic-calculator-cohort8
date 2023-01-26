7c43653f184f76d6d1c5f8e1f3844693726790cd
Christine Bai <bai20726@gmail.com>
2023-01-26 12:58:51 +0300

    used the switch case instead of using the class function

--- assets/classes/classes.php
+++ assets/classes/classes.php
@@ -1,17 +1,60 @@
 <?php
 
-class MyCalculator
-{
-    private $first_num;
-    private $second_num;
+// class MyCalculator
+// {
+//     private $first_num;
+//     private $second_num;
 
-    public function __construct($first_num, $second_num)
-    {
-        $this->first_num = $first_num;
-        $this->second_num = $second_num;
+//     public function __construct($first_num, $second_num)
+//     {
+//         $this->first_num = $first_num;
+//         $this->second_num = $second_num;
+//     }
+//             public function add() {
+//                 return $this->first_num + $this->second_num;
+//                 }
+//             public function subtract() {
+//                 return $this->first_num - $this->second_num;
+//                 }
+
+//             public function multiply() {
+//                 return $this->first_num * $this->second_num;
+//                 }
+//             public function divide() {
+//                 return $this->first_num / $this->second_num;
+//                     }
+// }
+
+if ( isset($_POST['submit']) ) {
+        
+    $val1 = $_POST['first_num'];
+    $val2 = $_POST['second_num'];
+
+    $opt = $_POST['results'];
+
+    $result = my_calculator($first_num, $second_num, $results);
+}
+
+function my_calculator($first_num, $second_num, $results) {
+    
+    switch($results) {            
+        case '+':
+            $value = $first_num + $second_num;
+            break;
+        case '-':
+            $value = $first_num - $second_num;
+            break;
+        case '*':
+            $value = $first_num * $second_num;
+            break;
+        case '/':
+            $value = $first_num / $second_num;
+            break;       
     }
+    return $value;
 }
 ?>
+
 
 
    
