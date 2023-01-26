d601dc7a8cf257c74a0ce29fea1641f93a0164d6
xubzero <allannjugush@gmail.com>
2023-01-26 15:04:35 +0300

    Removed reload script

--- index.php
+++ index.php
@@ -12,9 +12,6 @@ include "assets/classes/classes.php";
     <title>Simple-calculator</title>
 </head>
 
-<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
-
-
 <body>
 
 
