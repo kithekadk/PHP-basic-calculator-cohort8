76552b777d0c3878ad0601347125d691b208af60
Christine Bai <bai20726@gmail.com>
2023-01-26 15:03:18 +0300

    new code

--- assets/classes/classes.php
+++ assets/classes/classes.php
@@ -1,10 +1,10 @@
 <?php
 
-class myCalculator
-{
+class myCalculator{
     public $first_num;
     public $second_num;
     public $operator;
+    public $results;
 
     public function __construct($first_num, $second_num, $operator)
     {
@@ -13,35 +13,44 @@ class myCalculator
         $this->operator = $operator;
     }
 
-    public function my_calculator()
-    {
-        $results;
-        if ($this->operator == "+") {
-            $results = $this->first_num + $this->second_num;
-        } elseif ($this->operator == "-") {
-            $results = $this->first_num - $this->second_num;
-        } elseif ($this->operator == "*") {
-            $results = $this->first_num * $this->second_num;
-        } elseif ($this->operator == "/") {
-            $results = $this->first_num / $this->second_num;
+    public function my_calculator() {
+        if($this->operator=="+"){
+		    $this->results = $this->first_num + $this->second_num;
+        }elseif($this->operator=="-"){
+		    $this->results = $this->first_num - $this->second_num;
+        }elseif($this->operator=="*"){
+		    $this->results = $this->first_num * $this->second_num;
+        }elseif($this->operator=="/"){
+		    $this->results = $this->first_num / $this->second_num;
         }
     }
 
-    public function displayResults()
-    {
-        echo $results;
+    public function displayResults(){
+        echo $this->results;
     }
+
 }
 
 
-if (isset($_POST['Num1']) && isset($_POST['Num2'])) {
-    if (!empty($_POST['Num1']) && !empty($_POST['Num2'])) {
+if ( isset($_POST['Num1']) && isset($_POST['Num2'])) {
+    if(!empty($_POST['Num1']) && !empty($_POST['Num2'])){
         $theCalculation = new myCalculator($_POST["Num1"], $_POST["Num2"], $_POST['operator']);
         $theCalculation->my_calculator();
-        $theCalculation->displayResults();
     }
-} else {
+}else{
     echo "Please fill in both the first number and the second number";
 }
 
 ?>
+
+
+
+   
+
+  
+
+
+
+   
+
+  
